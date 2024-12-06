5a68221c018116eae12b80eaeb728a61741ed261
havanchau <chauhvc2.dy@gmail.com>
2024-12-06 13:56:00 +0700

    [fix] error UI

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,7 +1,6 @@
 import { FiSearch } from 'react-icons/fi';
 import Link from 'next/link';
 import './globals.css';
-import { Button } from '@nextui-org/react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { CartProvider } from './utils/Context';
@@ -56,9 +55,9 @@ export default async function RootLayout({ children }) {
               </div>
               <div className="w-1/2 h-full flex flex-grow justify-between items-center lg:place-self-end place-self-center">
                 <UserBtn />
-                <Button className="w-1/3 h-full flex items-center justify-center bg-transparent rounded-none relative font-semibold after:absolute after:h-0 after:w-0 after:bg-sky-800 after:left-1/2 overflow-hidden after:bottom-0 after:hover:h-1 after:hover:w-1 after:translate-x-full after:hover:scale-x-[300] after:hover:transition-all after:hover:duration-500 after:transition-all after:duration-500 transition-all duration-500">
+                <button className="w-1/3 h-full flex items-center justify-center bg-transparent rounded-none relative font-semibold after:absolute after:h-0 after:w-0 after:bg-sky-800 after:left-1/2 overflow-hidden after:bottom-0 after:hover:h-1 after:hover:w-1 after:translate-x-full after:hover:scale-x-[300] after:hover:transition-all after:hover:duration-500 after:transition-all after:duration-500 transition-all duration-500">
                   <FiSearch />
-                </Button>
+                </button>
                 <CartBtn/>
               </div>
             </div>
