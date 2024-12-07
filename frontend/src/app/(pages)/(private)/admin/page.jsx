70aa60e07640a7c59f0127021f05ef0de5d1c28b
Nguyễn Ngô Vũ Nhân <vunhan2492001@gmail.com>
2024-12-07 09:42:09 +0700

    Complete

--- frontend/src/app/(pages)/(private)/admin/page.jsx
+++ frontend/src/app/(pages)/(private)/admin/page.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import withAuth from "@/app/configs/route";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return <h1 className="text-3xl font-bold text-center mb-10">Admin Dashboard</h1>;
 };
 
 export default withAuth(Dashboard, ["admin"]);
